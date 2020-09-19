b7553b79fe4bd24991eac86929b4b4690fdeee49
Hocine <hoc1ne13dec@gmail.com>
2020-09-19 07:41:42 -0400

    sass

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#reserveCamp").click(function(){
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });  
+});
